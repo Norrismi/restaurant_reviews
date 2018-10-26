5b9607c218401d00404781f869a5e779b9620c18
Norrismi <Norrismi0@gmail.com>
2018-10-26 16:06:44 -0400

    modified gitingnore

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const config = {
+    production: {
+      SECRET: "process.env.SECRET",
+      DATABASE: "process.env.MONGODB_URI"
+    },
+    default: {
+      SECRET: "GQLnyt1ENGJfjZrzD2sYnEE5ZyxwS8PdL6AxyZWLjJ9wl1lyRU",
+      DATABASE: "mongodb://localhost:27017/Restaurant-DB"
+    }
+  };
+  
+  exports.get = function get(env) {
+    return config[env] || config.default;
+  };
+  
