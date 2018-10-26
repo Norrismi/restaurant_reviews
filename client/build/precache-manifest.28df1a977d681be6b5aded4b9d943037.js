26b8ac9351c5e86bb0d781a3f5bf68a355dca503
Norrismi <Norrismi0@gmail.com>
2018-10-26 14:56:30 -0400

    added build

---
+++ client/build/precache-manifest.28df1a977d681be6b5aded4b9d943037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4c810781f34960977c6",
+    "url": "/static/js/main.b4c81078.chunk.js"
+  },
+  {
+    "revision": "fb1f48309cdce571f6b2",
+    "url": "/static/js/1.fb1f4830.chunk.js"
+  },
+  {
+    "revision": "b4c810781f34960977c6",
+    "url": "/static/css/main.31fc5204.chunk.css"
+  },
+  {
+    "revision": "cac26a216b375775e44210f1c436ff07",
+    "url": "/index.html"
+  }
+];
