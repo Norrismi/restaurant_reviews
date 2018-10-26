392305257935246b1562a538ca75708b4efd0e25
Norrismi <Norrismi0@gmail.com>
2018-10-26 14:50:07 -0400

    changes to server.index

--- index.js
+++ index.js
@@ -6,18 +6,27 @@ const config = require("./config/config").get(process.env.NODE_ENV);
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DATABASE,{ useNewUrlParser: true })
-
+mongoose.connect(
+  config.DATABASE,
+  { useNewUrlParser: true }
+);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+require("./routes/restaurantRoutes")(app);
+require("./routes/userRoutes")(app);
 
-require('./routes/restaurantRoutes')(app)
-require('./routes/userRoutes')(app)
+//heroku
+app.use(express.static("client/build"));
 
-
-
+//heroku
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.get("/*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "./client", "build", "index.html"));
+  });
+}
 
 const port = process.env.port || 3001;
 
