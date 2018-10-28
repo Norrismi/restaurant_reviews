29de61b5d3c34dcc82057faee7fd6a9ce5e957bd
Norrismi <Norrismi0@gmail.com>
2018-10-27 21:06:19 -0400

    new DB setup added

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const config = require("./config/config").get(process.env.NODE_ENV);
+const keys = require("./config/keys")//.get(process.env.NODE_ENV);
 const app = express();
 
 mongoose.Promise = global.Promise;
+
 mongoose.connect(
-  config.DATABASE,
+  keys.mongoURI,
   { useNewUrlParser: true }
 );
 
