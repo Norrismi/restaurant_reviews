5c06edb74237363d044e44d551701be783b1ab59
Norrismi <Norrismi0@gmail.com>
2018-10-29 10:28:45 -0400

    changes to json and index for heroku

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ app.use(cookieParser());
 require("./routes/restaurantRoutes")(app);
 require("./routes/userRoutes")(app);
 
-//heroku
-app.use(express.static("client/build"));
 
 //heroku
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
   const path = require("path");
   app.get("/*", (req, res) => {
-    res.sendfile(path.resolve(__dirname, "./client", "build", "index.html"));
+    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
